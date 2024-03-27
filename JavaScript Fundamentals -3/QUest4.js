9aa144f91ba683cabf5b27efddb406809cd4f0ba
Utkarsh Tiwari <utkarshthor12@gmail.com>
2024-03-27 11:13:35 +0530

    Uploading JavaScript Fundamentals -3 Assignment

--- JavaScript Fundamentals -3/QUest4.js
+++ JavaScript Fundamentals -3/QUest4.js
@@ -1,17 +1,15 @@
 /* Develop a program that accepts an object and a property name, and checks if the object has the specified
 property. */
 
-
 const obj = {
-    x: 1,
-    y: 2,
-    z: 3,
-    a: 0,
+  x: 1,
+  y: 2,
+  z: 3,
+  a: 0,
 };
 
-function checkProp(obj, prop)
-{
-    return obj.hasOwnProperty(prop)
+function checkProp(obj, prop) {
+  return obj.hasOwnProperty(prop);
 }
 
 console.log(checkProp(obj, "x"));
